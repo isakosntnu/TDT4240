c538b94cc3b077cc28db21703f914866f34c0900
Isak Olav Sjøberg <isakos@stud.ntnu.no>
2025-04-02 10:56:36 +0200

    Fix: updated background and added Title

--- core/src/com/drawguess/game/view/MainMenuView.java
+++ core/src/com/drawguess/game/view/MainMenuView.java
@@ -1,9 +1,11 @@
 package com.drawguess.game.view;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.drawguess.game.WordBattle;
@@ -15,51 +17,50 @@ public class MainMenuView {
     private Rectangle joinGameButtonBounds;
     private Rectangle newGameButtonBounds;
     private OrthographicCamera cam;
-
-
-
-
+    private BitmapFont titleFont;
 
     public MainMenuView(OrthographicCamera cam) {
         this.cam = cam;
-        this.backgroundTexture = new Texture("bg2.png");
-        this.joinGameButton = new Texture("join_game.png");
+        this.backgroundTexture = new Texture("bakgrunn.jpg"); // Change to vertical-friendly background
+        this.joinGameButton = new Texture("join_game.png"); // Updated buttons
         this.newGameButton = new Texture("new_game.png");
 
+        titleFont = new BitmapFont();
+        titleFont.setColor(Color.WHITE); // Title color
+        titleFont.getData().setScale(3); // Adjust size
 
-        // Calculate bounds and assign them
+        // Position elements for vertical screen
+        float screenWidth = WordBattle.WIDTH;
+        float screenHeight = WordBattle.HEIGHT;
+
         joinGameButtonBounds = new Rectangle(
-                (WordBattle.WIDTH - joinGameButton.getWidth()) / 2,
-                (WordBattle.HEIGHT / 2 - joinGameButton.getHeight() / 2) - 50,
+                (screenWidth - joinGameButton.getWidth()) / 2,
+                screenHeight * 0.5f, // Center vertically
                 joinGameButton.getWidth(),
                 joinGameButton.getHeight()
         );
 
         newGameButtonBounds = new Rectangle(
-                (WordBattle.WIDTH - newGameButton.getWidth()) / 2,
-                joinGameButtonBounds.y - newGameButton.getHeight() - 20, // Adjust y-position based on joinGameButton's position
+                (screenWidth - newGameButton.getWidth()) / 2,
+                joinGameButtonBounds.y - newGameButton.getHeight() - 50, // Spacing below
                 newGameButton.getWidth(),
                 newGameButton.getHeight()
         );
     }
 
     public void render(SpriteBatch spriteBatch) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(0.1f, 0.2f, 0.5f, 1); // Dark blue background
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         spriteBatch.begin();
-        // Render background to fill the screen
         spriteBatch.draw(backgroundTexture, 0, 0, WordBattle.WIDTH, WordBattle.HEIGHT);
-        // Render buttons using bounds for precise alignment
+        titleFont.draw(spriteBatch, "DrawGuess", WordBattle.WIDTH * 0.35f, WordBattle.HEIGHT - 100); // Title at top
+
         spriteBatch.draw(joinGameButton, joinGameButtonBounds.x, joinGameButtonBounds.y);
         spriteBatch.draw(newGameButton, newGameButtonBounds.x, newGameButtonBounds.y);
         spriteBatch.end();
     }
 
-
-
-
-    // Provide a way for the controller to access the bounds
     public Rectangle getJoinGameButtonBounds() {
         return joinGameButtonBounds;
     }
@@ -72,6 +73,6 @@ public class MainMenuView {
         backgroundTexture.dispose();
         joinGameButton.dispose();
         newGameButton.dispose();
+        titleFont.dispose();
     }
 }
-
